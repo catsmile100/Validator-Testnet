71557e619a01ed53c8eb2645379fac2e4be05515
Catsmile Project <85368621+catsmile100@users.noreply.github.com>
2024-12-05 01:25:34 +0700

    Update mini.sh

--- Story/mini.sh
+++ Story/mini.sh
@@ -1,66 +1,37 @@
 #!/bin/bash
 
 echo "Cleaning up old installation..."
-
-# Stop services if they exist
 sudo systemctl stop story story-geth 2>/dev/null
 sudo systemctl disable story story-geth 2>/dev/null
-
-# Remove old services
 sudo rm -f /etc/systemd/system/story.service
 sudo rm -f /etc/systemd/system/story-geth.service
 sudo systemctl daemon-reload
-
-# Remove old directories and files
 rm -rf $HOME/.story
 rm -rf $HOME/story
 rm -rf $HOME/go/bin/story
 rm -rf $HOME/go/bin/geth
 
-# Remove old Go installation
-sudo rm -rf /usr/local/go
-
-echo "Cleanup completed. Starting fresh installation..."
-
-# Vars
-MONIKER="test"
-CHAIN_ID="odyssey-0"
-
 # Install dependencies
 sudo apt update
-sudo apt install curl git wget htop tmux build-essential jq make gcc unzip -y
+sudo apt install curl wget jq lz4 -y
 
-# Install Go v1.21.5
-cd $HOME
-VER="1.21.5"
-wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
-sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
-rm "go$VER.linux-amd64.tar.gz"
-echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
-source ~/.bash_profile
-[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
-
-# Install Story
-cd $HOME
-wget -O geth https://github.com/piplabs/story-geth/releases/download/v0.10.1/geth-linux-amd64
+# Get latest release for Story Geth
+GETH_LATEST=$(curl -s https://api.github.com/repos/piplabs/story-geth/releases/latest | jq -r .tag_name)
+wget -O geth "https://github.com/piplabs/story-geth/releases/download/$GETH_LATEST/geth-linux-amd64"
 chmod +x geth
 sudo mv geth /usr/local/bin/
 
+# Get latest release for Story
+cd $HOME
 git clone https://github.com/piplabs/story
 cd story
-git checkout v0.13.0
-go mod tidy
-go install ./client    
+STORY_LATEST=$(git describe --tags $(git rev-list --tags --max-count=1))
+git checkout $STORY_LATEST
+go build -o story ./client
+sudo mv story /usr/local/bin/
 
-# Verifikasi instalasi
-which story
-if [ $? -ne 0 ]; then
-    echo "Story installation failed!"
-    exit 1
-fi
-
-# Init
-story init $MONIKER --chain-id $CHAIN_ID
+# Init Story
+story init test --chain-id odyssey-0
 
 # State Sync
 SNAP_RPC="https://story-testnet-rpc.itrocket.net:443"
@@ -68,10 +39,8 @@ LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height)
 BLOCK_HEIGHT=$((LATEST_HEIGHT - 1000))
 TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
 
-# Create config directory if it doesn't exist
-mkdir -p $HOME/.story/story/config
-
 # Configure
+mkdir -p $HOME/.story/story/config
 sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
 s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
 s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
@@ -85,7 +54,7 @@ After=network-online.target
 
 [Service]
 User=$USER
-ExecStart=$(which story) run
+ExecStart=/usr/local/bin/story run
 Restart=always
 RestartSec=3
 LimitNOFILE=65535
@@ -101,7 +70,7 @@ After=network-online.target
 
 [Service]
 User=$USER
-ExecStart=$(which geth) --odyssey --syncmode full --http
+ExecStart=/usr/local/bin/geth --odyssey --syncmode full --http
 Restart=always
 RestartSec=3
 LimitNOFILE=65535
@@ -110,7 +79,6 @@ LimitNOFILE=65535
 WantedBy=multi-user.target
 EOF
 
-# Start services
 sudo systemctl daemon-reload
 sudo systemctl enable story story-geth
 sudo systemctl start story story-geth
